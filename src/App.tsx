8b04144dc1f772ba0287799bc337ca17a0664d0b
Enrico Elizan Jr <eelizanjr@outlook.com>
2023-01-15 12:31:19 -0500

    Added basic shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Hi</h1>} />
+        <Route path="/new" element={<h1>Home</h1>} />
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/:id">
+          <Route index element={<h1>show</h1>} />
+          {/* .../id/edit */}
+          <Route path="edit" element={<h1>edit</h1>} />
+        </Route>
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
